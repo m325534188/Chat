1737cfab05b033f06af24bc2ec8f4e05ea78c93c
Miriam Avranovits <m548532941@gmail.com>
2025-05-21 20:56:09 +0300

    end

--- server/index.js
+++ server/index.js
@@ -19,6 +19,12 @@ app.use('/users', userRouter);
 
 app.use('/messages', messageRouter); 
 
-app.listen(5000, 'localhost', () => {
-  console.log("Server running on http://localhost:5000");
+// app.listen(5000, 'localhost', () => {
+//   console.log("Server running on http://localhost:5000");
+// });
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
