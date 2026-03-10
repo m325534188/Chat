3cbe9fb8d8fd5f8acd5da6695e9b4977b3894580
m325534188 <m548532941@gmail.com>
2026-03-10 17:03:12 +0200

    update uri

--- server/main.js
+++ server/main.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 //connect db
-mongoose.connect("mongodb+srv://username:password@cluster0.tvqct.mongodb.net/yourDB?retryWrites=true&w=majority", { 
+mongoose.connect("mongodb+srv://admin:chat%21491@cluster0.tvqct.mongodb.net/miri?retryWrites=true&w=majority", { 
   useNewUrlParser: true, 
   useUnifiedTopology: true 
 }).then(() => console.log("Connected to MongoDB"))
